3ca9971e1593d3f15e1d491b0ada6c7253b0f124
JoosCode <atenciosystem@hotmail.com>
2021-01-09 10:42:32 -0400

    alpha version

--- README.md
+++ README.md
@@ -1,7 +1,62 @@
 # jooscode
 
-## test package
+## My first contribution, simple https service provider to optimize works.
+
+### Pre requirements 📋
+
+- [Vue.js](https://vuejs.org/)
+
+### Install 🔧
 
 ```
-test package
+npm i servih
+```
+
+_or_
+
+```
+yarn add servih
+```
+
+### ⌨️
+
+_in main.js add servihOptions._
+
+```
+import {servihOptions} from 'servih'
+
+servihOptions('token');
+
+```
+
+_in your component, or store use. Has support (get, post, put, delete)_
+
+```
+import {serviHttp} from 'servih'
+
+export default {
+
+   mounted() {
+       this.test();
+   },
+
+    methods:{
+        test:function(){
+             serviHttp('posts', 'post', {name:'josedaniel'}).then(response=>{
+                console.log(response);
+                }).catch(errors=>{
+                console.log(errors);
+            });
+        }
+    }
+
+}
+```
+
+```
+
+```
+
+```
+
 ```
--- index.js
+++ index.js
@@ -1,5 +1,100 @@
-function HelloWordJooscode() {
-    console.log('aqui');
+
+var axios = require('axios');
+const url = require("../../.env.js");
+var tokenGlobal = "";
+var optionGlobal = "";
+
+
+
+
+if (url) {
+    function servihOptions(token){
+        tokenGlobal = token;
+        //optionGlobal = options;
+    }    
+}else{
+    console.log('add .env.js in root')
 }
 
-export default HelloWordJooscode
+
+if (url) {
+    function serviHttp(path, methods, form){
+        
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Requested-With": "XMLHttpRequest",
+                        Authorization: "Bearer " + tokenGlobal
+                    }
+                };
+
+                return new Promise((res, rej) => {
+                    methods == 'get'?
+                    
+                    axios
+                      .get(
+                        `${url}/${path}`,form ,config
+                      )
+                      .then(response => {
+                        res(response.data);
+                        
+                      })
+                      .catch(err => {
+                        rej("error al subir " + err);
+                      })
+                      :null
+                      methods == 'post'?
+                    
+                    axios
+                      .post(
+                        `${url}/${path}`,form ,config
+                      )
+                      .then(response => {
+                        res(response.data);
+                        
+                      })
+                      .catch(err => {
+                        rej("error al subir " + err);
+                      })
+                      :null
+                      methods == 'put'?
+                    
+                    axios
+                      .put(
+                        `${url}/${path}`,form ,config
+                      )
+                      .then(response => {
+                        res(response.data);
+                        
+                      })
+                      .catch(err => {
+                        rej("error al subir " + err);
+                      })
+                      :null
+                      methods == 'delete'?
+                    
+                    axios
+                      .delete(
+                        `${url}/${path}`,form ,config
+                      )
+                      .then(response => {
+                        res(response.data);
+                        
+                      })
+                      .catch(err => {
+                        rej("error al subir " + err);
+                      })
+                      :null
+                });
+        
+        
+        
+    }    
+}else{
+    console.log('add .env.js in root')
+}
+
+
+
+exports.servihOptions = servihOptions;
+exports.serviHttp = serviHttp;
